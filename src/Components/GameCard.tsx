ce0ce429a097f12a3be5403ced37710bfadaf767
Francois Marvens <fmarvens11@gmail.com>
2024-03-14 17:10:22 -0400

    Displaying platform icons

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import {} from "react-icons/fa";
 import { Game } from "../hooks/useGames";
-import { Image, CardBody, Heading, Card } from "@chakra-ui/react";
+import { Image, CardBody, Heading, Card, Text } from "@chakra-ui/react";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
   game: Game;
@@ -8,12 +10,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-      </CardBody>
-    </Card>
+    <>
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={game.background_image} />
+        <CardBody>
+          <Heading fontSize="2xl">{game.name}</Heading>
+
+          <PlatformIconsList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+        </CardBody>
+      </Card>
+    </>
   );
 };
 
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import logo from "../assets/Let gmaing.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="100px" />
       <ColorModeSwitch />
     </HStack>
   );
